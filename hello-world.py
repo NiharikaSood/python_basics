10dc377b4bcd962463e9c18423119a024785610a
NiharikaSood <niharikasood92@gmail.com>
2023-08-25 12:04:03 +0530

    add two numbers.

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,17 @@
 print("Hello World!!")
+
+# Sum of two numbers using user input.
+
+# By default the input() function takes the values as a string. So, even if it is number still it will consider it as string.
+# So, in this scenario we have to do is typecasting in python.
+#  What is typecasting?
+# Typecasting is basicasically converting one data type to another.
+
+
+
+
+a = input("Enter first number : ")
+b = input("Enter second number : ")
+
+# sum = a + b
+print( int(a) + int(b))
---
+++ sum-two-no.py
@@ -0,0 +1,3 @@
+a = input("enter the first no.")
+b = input("enter the second n0.")
+print(int(a) + int(b))
