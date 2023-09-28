4801eceb37243e60914dc3b1dd7a77fc42c13a86
NiharikaSood <niharikasood92@gmail.com>
2023-09-28 10:35:56 +0530

    arithmetic operator

---
+++ arithmetic-operator.py
@@ -0,0 +1,80 @@
+#addition
+print(10 + 5)
+ 
+float1 = 13.25
+float2 = 3.40
+print(float1 + float2)
+
+num = 20
+flt = 10.4
+print(num + flt)
+
+
+#subtraction
+print(10 - 5)
+
+float1 = -18.678
+float2 = 3.55
+print(float1 - float2)
+
+num = 20
+flt = 10.5
+print(num - flt)
+
+
+#multiplication
+print(40 * 10)
+
+float1 = 5.5
+float2 = 4.5
+print(float1 * float2)
+
+print(10.2 * 3)
+
+
+#division
+print(40/10)
+
+float1 = 5.5
+float2 = 4.5
+print(float1/float2)
+
+print(12.4/2)
+
+
+#floor division
+print(43 // 10)
+
+float1 = 5.5
+float2 = 4.5
+print(float1 // float2)
+
+print(12.4 // 2)
+
+
+#modulo
+print(10 % 2)
+
+print(-28 % 10) #the remainder is positive if the right hand operand is positive
+
+print(28 % -10) #the remainder is negative if the right hand operand is negative
+
+print(34.4 % 2.5) #the remainder can be float
+
+twenty_eight = 28
+print(twenty_eight % 10)
+
+
+#precedence
+
+#different precedence
+print(10 - 3 * 2) #multiplication computed first followed by subtraction
+ #same precedence
+print(3 * 20 / 5) #multiplication computed first followed by division
+print(3 / 20 * 5) #division computed first followed by multiplication
+
+
+#parentheses
+print((10 -3) * 2) #subtraction occur first
+
+print((18 +2)/(10/8))
