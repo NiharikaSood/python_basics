4b2da2950f6e51375b8a2ad4a2a6307f4b3d72dc
NiharikaSood <niharikasood92@gmail.com>
2023-09-28 17:37:28 +0530

    assignment operator

---
+++ assignment-operator.py
@@ -0,0 +1,17 @@
+first = 20
+second = first
+first = 35
+print(first, second)
+
+num=10
+print(num)
+num+=5
+print(num)
+num-=5
+print(num)
+num*=2
+print(num)
+num/=2
+print(num)
+num**=2
+print(num)
