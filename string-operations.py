adab5ec4ad6e54c867cafb16a72ec997a6fdd136
NiharikaSood <niharikasood92@gmail.com>
2023-10-01 18:41:03 +0530

    string operations

---
+++ string-operations.py
@@ -0,0 +1,25 @@
+#comparison operator
+print('a' < 'b')
+
+house = "gryffindor"
+house_copy = "gryffindor"
+print(house == house_copy)
+new_house = "slytherin"
+print(house == new_house)
+print(new_house <= house)
+print(new_house >= house)
+
+
+#concatenation
+first_half = "bat"
+second_half = "man"
+full_name = first_half + second_half
+print(full_name)
+
+print("ha"*3)
+
+
+#search
+random_string = "this is a random string"
+print('of' in random_string)
+print('random' in random_string)
