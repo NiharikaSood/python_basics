dd78245ee59e90c35e527383265a92d5c6418078
NiharikaSood <niharikasood92@gmail.com>
2023-10-06 18:11:12 +0530

    condition with logical operator

---
+++ condition-with-logical-operator.py
@@ -0,0 +1,5 @@
+num = 12
+if num%2==0 and num%3==0 and num%4==0:
+    print(" The number is a multiple of 2,3 and 4")
+if(num%5==0 or num%6==0):
+    print(" The number is a multiple of 5 and/or 6")    
