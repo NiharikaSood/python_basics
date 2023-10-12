a2e24d4b282a5ac5f938fd40d6499cf75ca557f0
NiharikaSood <niharikasood92@gmail.com>
2023-10-12 18:10:14 +0530

    nested if

--- nested-if.py
+++ nested-if.py
@@ -3,3 +3,11 @@ if num>=0 and num<=100:
     if num>=50 and num<=75:
         if num>=60 and num<=70:
             print(" The no. is in the 60-70 range ")
+
+# Creating and editing value
+num = 10
+if num>5:
+    num=20
+    new_num=num*5
+    print(num)
+    print(new_num)            
